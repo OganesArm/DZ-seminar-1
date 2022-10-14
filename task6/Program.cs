56c645d245bd1c78a0d98dea08c47896aec2a075
Oganes <ov_ovanisan@outlook.com>
2022-10-14 15:44:06 +0300

    end

--- task6/Program.cs
+++ task6/Program.cs
@@ -3,7 +3,7 @@ int a = Convert.ToInt32(Console.ReadLine());
 if (a%2 ==0){ 
 Console.WriteLine("Число "+a + " четное");
 } else {
-Console.WriteLine("Число "+a + " НЕ четное");
+Console.WriteLine("Число "+a + " НЕчетное");
 }
 
 
