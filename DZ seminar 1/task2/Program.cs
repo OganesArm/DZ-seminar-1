af6542b876b6bd7ee20f626ca7ebcf381ec4345f
Oganes <ov_ovanisan@outlook.com>
2022-10-14 13:14:44 +0300

    1

---
+++ DZ seminar 1/task2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, 
+//а какое меньшее.
+
+Console.WriteLine("Введите первое число до 7"); 
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число до 7"); 
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b && a<8 && b<8) {
+Console.WriteLine("Число "+a + " больше");
+} 
+else if (a<b && b<8 && a<8) { 
+Console.WriteLine("Число "+b + " больше");
+} 
+else if (a==b && b <8 && a <8) {
+Console.WriteLine("Числа равны"); 
+} 
+else {
+    Console.WriteLine("Ошибка ввода. Повторите попытку");
+} 
---
+++ DZ seminar 1/task4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Пожалуйста, введите три числа и наша система найдет максимальное число. Учтите, значение любого числа не может превышать 78"); 
+Console.WriteLine("Введите первое число"); 
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число"); 
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число"); 
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a>b && a>c && a<79) {
+Console.WriteLine("Число "+a + " максимальное");
+}
+else if (b>a && b>c && b<79) {
+    Console.WriteLine("Число "+b + " максимальное");
+}
+else if (c>a && c>b && c<79) {
+    Console.WriteLine("Число "+c + " максимальное");
+}
+else {
+    Console.WriteLine("Ошибка ввода. Число не должно превышать 78. Повторите попытку");
+} 
